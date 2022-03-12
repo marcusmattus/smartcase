5768007712a1241c2b4ffa3f14588f1c6ee4b3b3
Marcus Mattus <57033847+marcusmattus@users.noreply.github.com>
2022-03-12 16:22:46 +0000

    commint chanes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import './App.css';
-import {Text} from 'react';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Text>hello</Text>
-      </header>
+      <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
     </div>
   );
 }
