0a0de20b0d349a2f77d918753d1fb98bb2595baa
Marcus Mattus <57033847+marcusmattus@users.noreply.github.com>
2022-03-12 15:40:34 +0000

    change hello

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Text} from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Text>hello</Text>
       </header>
     </div>
