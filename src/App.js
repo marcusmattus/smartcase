5d6611cd9a512d982d663bcc71b257d244db27f3
Marcus Mattus <57033847+marcusmattus@users.noreply.github.com>
2022-03-12 17:09:12 +0000

    update4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/NavBar';
+import Navbar from './component/NavBar/index';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages';
 
